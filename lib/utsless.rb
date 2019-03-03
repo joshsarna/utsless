92b541472cc0801173de280a1231245be3ba661b
joshsarna <joshsarna@gmail.com>
2019-03-02 21:18:46 -0800

    add namespaced class_tree method that returns array

--- lib/utsless.rb
+++ lib/utsless.rb
@@ -5,11 +5,24 @@ end
 module Utsless
   def self.print_class_tree(x)
     c = x
+    class_list = [c]
     p "*" * 25 + " Class Tree of #{x} " + "*" * 25
     while c != Class
       c = c.class
       p c
+      class_list << c
     end
     p "*" * (66 + x.to_s.length)
+    class_list
+  end
+
+  def self.class_tree(x)
+    c = x
+    class_list = [c]
+    while c != Class
+      c = c.class
+      class_list << c
+    end
+    class_list
   end
 end
